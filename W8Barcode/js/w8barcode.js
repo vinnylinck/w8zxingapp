8fbfdbff5d615fe0b76db332c264dc65eeeb0595
Vinicius Linck <vinnylinck@gmail.com>
2013-12-18 18:40:49 -0200

    Added "video sampling" and capture photo feature,

--- W8Barcode/js/w8barcode.js
+++ W8Barcode/js/w8barcode.js
@@ -1,82 +1,41 @@
-﻿var canvas, pid, stopped, sampler, settings, mediaDevices, myDevices, localFolder, photoStorageFile, imageProps;
+﻿// Using
+var Capture = Windows.Media.Capture,
 
 
-function render() {
-    if (!stopped) {
+// Globals
+    previewPanel,
+    sampler,
+    memoryBuffer;
 
-        sampler.capturePhotoToStorageFileAsync(imageProps, photoStorageFile);
 
-    }
-}
-
+// start
 function start() {
-    if (stopped) {
-        stopped = false;
-        pid = window.requestAnimationFrame(render);
-    }
+    previewPanel.src = URL.createObjectURL(sampler);
+    previewPanel.play();
 }
 
+// stop
 function stop() {
-    if (pid) {
-        window.cancelAnimationFrame(pid);
-    }
-    stopped = true;
+    previewPanel.pause();
+    previewPanel.src = null;
 }
 
-function init() {
-    stopped = true;
-    localFolder = Windows.Storage.ApplicationData.current.localFolder;
-    
-    sampler = new Windows.Media.Capture.MediaCapture();
-    settings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
+// flick
+function flick() {
+    var photoProperties = Windows.Media.MediaProperties.ImageEncodingProperties.createJpeg();
+        
+    memoryBuffer  = new Windows.Storage.Streams.InMemoryRandomAccessStream();
+    sampler.capturePhotoToStreamAsync(photoProperties, memoryBuffer).done(function onDone() {
+        console.log("Image sample collected! ");
 
-    settings.videoDeviceId = myDevices.getAt(0).id;
-    sampler.initializeAsync(settings).then(
-        function onSuccess() {
-            console.log('Camera has been initialized!');
-        },
-        function onError() {
-            var msg = new Windows.UI.Popups.MessageDialog("Error initializing camera!");
-            msg.showAsync();
-        }
-    );
-
-    localFolder.createFileAsync("W8BARCODE.jpg", Windows.Storage.CreationCollisionOption.replaceExisting).then(
-        function onSuccess(sampleFile) {
-            photoStorageFile = sampleFile;
-        },
-        function onError() {
-            var msg = new Windows.UI.Popups.MessageDialog("Error creating sample file!");
-            msg.showAsync();
-        }
-    );
-
-
-    imageProps = Windows.Media.MediaProperties.ImageEncodingProperties();
-    imageProps.subtype = "JPEG";
-    imageProps.width  = 640;
-    imageProps.height = 480;
-
-    canvas = document.getElementById("samplePanel");
+    });
 }
 
-// bootstrapping plugin
-mediaDevices = Windows.Devices.Enumeration.DeviceInformation;
-mediaDevices.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture).then(
-    function onSuccess(deviceInformationCollection) {
-        var msg;
+function initPreview() {
+    previewPanel = document.getElementById("imagePreview");
+    sampler = new Capture.MediaCapture();
+    sampler.initializeAsync();
+}
 
-        myDevices = deviceInformationCollection;
-
-        if (myDevices.length !== 1) {
-            msg = new Windows.UI.Popups.MessageDialog("No Camera found!");
-            msg.showAsync();
-        } else {
-            init();
-        }
-    },
-    function onError() {
-        var msg = new Windows.UI.Popups.MessageDialog("Error detecting camera device!");
-        msg.showAsync();
-    }
-);
+// hooking preview initialization
+document.addEventListener("DOMContentLoaded", initPreview);
